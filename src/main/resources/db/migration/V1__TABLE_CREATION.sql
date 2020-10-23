11332eaec9cec464f5dfe42599b8c6ab538ee852
Lukas <lukas@leucali.net>
2020-10-23 14:21:42 +0200

    Introduced Flyway

---
+++ src/main/resources/db/migration/V1__TABLE_CREATION.sql
@@ -0,0 +1,12 @@
+CREATE TABLE car (
+    id int PRIMARY KEY,
+    marke VARCHAR,
+    model VARCHAR,
+    gewicht int,
+    leistung int,
+    drehmoment int,
+    farbe VARCHAR,
+    tueren int,
+    klasse VARCHAR,
+    motor_art VARCHAR
+);
---
+++ src/main/resources/db/migration/V2__CREATE_DATA.sql
@@ -0,0 +1,8 @@
+insert into car (id, marke, model, gewicht, leistung, drehmoment, farbe, tueren, klasse, motor_art)
+values (1000, 'Mercedes', 'A-Klasse', 1200, 120, 400, 'grau', 3, 'Limousine', 'Benzin');
+
+insert into car (id, marke, model, gewicht, leistung, drehmoment, farbe, tueren, klasse, motor_art)
+values (1001, 'BMW', 'M4', 2000, 520, 600, 'gruen', 3, 'Coupe', 'Benzin');
+
+insert into car (id, marke, model, gewicht, leistung, drehmoment, farbe, tueren, klasse, motor_art)
+values (1002, 'Audi', 'A4', 1100, 150, 200, 'rot', 5, 'Kombi', 'Diesel');

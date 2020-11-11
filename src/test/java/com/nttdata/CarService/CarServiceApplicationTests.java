563068dd6fa2b062f7618262fe27a51ecf8c1fd1
Lukas <lukas@leucali.net>
2020-11-11 10:56:34 +0100

    Fixed Test Springboot

--- src/test/java/com/nttdata/CarService/CarServiceApplicationTests.java
+++ src/test/java/com/nttdata/CarService/CarServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.nttdata.CarService;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CarServiceApplicationTests {
 
 	@Test
